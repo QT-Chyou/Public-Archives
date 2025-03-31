e58bc84f2b08ac2864176e4a2bc34f1e93c01d6b
Au.Chyou <chyou.eighteen@gmail.com>
2025-03-31 11:09:48 +0800

    Update MorseCode.ino

--- MorseCode.ino
+++ MorseCode.ino
@@ -1,18 +1,20 @@
 /*
 MORSE CODE TRANSLATOR
 Hi, I've included some comments on each segment to explain each purpose and how they work
-change the variable according to what you want, I've used this because it's confusing and helps me to keep on differentitating things
+change the variable according to what you want, I've used this because it's confusing and helps me to keep on differentiating things
 - P.S: The Autumn Engineer
 */
 
 String rem = "";
 const int button = 2;
 int reading = 0;
-int counter = 0; int bounce = 0; int wordtimer = 0;
+int counter = 0; 
+int bounce = 0; 
+int wordtimer = 0;
 String morse = "";
 String ren = "";
-String period = String(".");
-String dash = String("-");
+String period = ".";
+String dash = "-";
 
 String morseAlphabet[] = { //Yk what this is (✿╹◡╹)
       ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", 
@@ -30,8 +32,8 @@ void setup() {
 
 void loop() {
   reading = digitalRead(button);
-  if (reading == LOW) { //checks when the button is not pressed
-    counter++; bounce = 0;
+  if (reading == LOW) {
+    counter++; bounce = 0; wordtimer = 0;
     delay(10);
   } 
   else if (reading == HIGH && counter > 0) { //checks if the button is not pressed and will check the input 
@@ -42,30 +44,33 @@ void loop() {
     counter = 0; bounce = 1;
   }
 
-  if (reading == HIGH && bounce > 0) {
-    bounce++; wordtimer++; //starts the bouncer for the precedding segment
+  if (reading == HIGH && bounce > 0) { //starts the bouncer for the precedding segment
+    bounce++; 
+    wordtimer++; 
   }
 
-  if (bounce == 30) {
-    if (rem == "") { //should add a space, yet to update(✿╹◡╹)..
-      ren += " ";
-    }
+  if (bounce == 30 && reading == HIGH) {
     for (int i = 0; i < 26; i++) { //we iterate on each to find a match and return it
       if (rem == morseAlphabet[i]) {
         Serial.println(alphabet[i]);
         ren += alphabet[i]; rem = "";
-        break;  
+        break;
       } 
     }
     rem = ""; //resets once it did not find a match
   } 
 
-  //This segment checks if you paused for a while and then prints out your word
-  if (wordtimer == 120) {
+  if (bounce == 100) { //induces a space, the most efficient one I found so far (ￗ﹏ￗ )
+    ren += " "; Serial.println("space");
+  }
+
+  //This segment checks if you paused for print and then prints out your word
+  if (wordtimer == 180 && reading == HIGH) {
     Serial.println(ren);  
     ren = "";
     bounce = 0; 
     wordtimer = 0;
+    rem = "";
   }
   delay(10); //necessary, to avoid conflicting inputs
 }
